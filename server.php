a62ae134b720d542c97aadf20cada60aaf49baea
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2024-12-19 21:29:34 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,21 +1,15 @@
 <?php
-// server.php
 
-// Mengatur header untuk JSON
 header('Content-Type: application/json');
 
-// Mengecek apakah ada permintaan GET atau POST
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    // Mengambil data request.json
     $requests = file_get_contents('request.json');
     echo $requests;
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Ambil data POST
     $input = json_decode(file_get_contents('php://input'), true);
 
-    // Menyimpan data permintaan ke request.json
     $requests = json_decode(file_get_contents('request.json'), true);
-    $requests[] = $input; // Menambahkan permintaan baru ke array
+    $requests[] = $input;
 
     file_put_contents('request.json', json_encode($requests, JSON_PRETTY_PRINT));
 
